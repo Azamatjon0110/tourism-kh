cc54265fa35bd47fb6347f5f83dd3d2cc04fe9f5
Azamatjon0110 <abdurazaqovazamatjon20011003@gmail.com>
2024-04-15 11:13:04 +0500

    Add bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import '../src/assets/bootstrap/bootstrap.min.js';
 import './index.css';
+import 'bootstrap/dist/js/bootstrap.js';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 
